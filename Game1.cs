5501cef803ffdeb30b24629e8ed7f1198daa2b25
Miracle McMahon <339663114@GoTVDSB.ca>
2025-05-02 14:42:27 -0400

    I guess trying to make more stuff

--- Game1.cs
+++ Game1.cs
@@ -20,7 +20,11 @@ namespace Mono_Summative_Assignment___Breakout_Game
 
         Texture2D paddleTexture;
         Texture2D ballTexture;
+
+        Texture2D introBackground;
         Texture2D galaxyBackground;
+        Texture2D goodBackground;
+        Texture2D badBackground;
 
         Rectangle ballRect;
         Rectangle brickRect;
@@ -33,7 +37,10 @@ namespace Mono_Summative_Assignment___Breakout_Game
         List<Brick> brickPlacer;
         List<Texture2D> brickTextures;
 
+        SpriteFont textFont;
+
         KeyboardState keyboardState;
+        Screen screenState;
 
         //Things to do
         //Make a Ball Class and give it all of its functions
@@ -50,6 +57,7 @@ namespace Mono_Summative_Assignment___Breakout_Game
         {
             // TODO: Add your initialization logic here
 
+            screenState = Screen.Game;
             paddleRect = new Rectangle(0, 0, 25, 25);
             brickPlacer = new List<Brick>();
             window = new Rectangle(0, 0, 800, 500);
@@ -76,7 +84,13 @@ namespace Mono_Summative_Assignment___Breakout_Game
             paddleTexture = Content.Load<Texture2D>("Images/paddle");
             ballTexture = Content.Load<Texture2D>("Images/marble");
             brickTextures.Add(Content.Load<Texture2D>("Images/crystal_brick"));
+
+            textFont = Content.Load<SpriteFont>("Fonts/TextFont");
+
             galaxyBackground = Content.Load<Texture2D>("Images/purple_galaxy");
+            introBackground = Content.Load<Texture2D>("Images/intro_background");
+            goodBackground = Content.Load<Texture2D>("Images/good_end_background");
+            badBackground = Content.Load<Texture2D>("Images/bad_end_background");
         }
 
         protected override void Update(GameTime gameTime)
@@ -99,11 +113,27 @@ namespace Mono_Summative_Assignment___Breakout_Game
 
             // TODO: Add your drawing code here
             _spriteBatch.Begin();
-            _spriteBatch.Draw(galaxyBackground, window, Color.White);
-            foreach (Brick brick in brickPlacer)
-                brick.Draw(_spriteBatch);
-            paddle.Draw(_spriteBatch);
-            ball.Draw(_spriteBatch);
+            if (screenState == Screen.Intro)
+            {
+                _spriteBatch.Draw(introBackground, window, Color.White);
+                _spriteBatch.DrawString(textFont, "Breakout", new Vector2(400, 10), Color.Black);
+            }
+            else if (screenState == Screen.Game)
+            {
+                _spriteBatch.Draw(galaxyBackground, window, Color.White);
+                foreach (Brick brick in brickPlacer)
+                    brick.Draw(_spriteBatch);
+                paddle.Draw(_spriteBatch);
+                ball.Draw(_spriteBatch);
+            }
+            else if (screenState == Screen.GoodEnd)
+            {
+                _spriteBatch.Draw(goodBackground, window, Color.White);
+            }
+            else
+            {
+                _spriteBatch.Draw(badBackground, window, Color.White);
+            }
             _spriteBatch.End();
 
             base.Draw(gameTime);
